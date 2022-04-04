536612c65080403f54a679a5f84a5c7a9b9b191f
aluno <aluno@DSN1251017041>
2022-04-04 10:56:23 -0300

    Segundo commit

--- TesteGitZero/src/Testee.java
+++ TesteGitZero/src/Testee.java
@@ -2,8 +2,13 @@
 public class Testee {
 
 	public static void main(String[] args) {
-		System.out.println("Teste coisa linda");
+		System.out.println("Sorvete é muito bom");
 
+		
+		
+		
+		
+		
 	}
 
 }
