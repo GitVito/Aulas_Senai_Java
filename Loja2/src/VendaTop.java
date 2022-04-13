176d966fecdd4e7678f2b8f6e27631447655806e
aluno <aluno@DSN1251017041>
2022-04-13 08:32:04 -0300

    VendaTop, salvando para não perder   não finalizado
      programa para a compra de produtos e ter um desconto.

---
+++ Loja2/src/VendaTop.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class VendaTop {
+
+	public static void main(String[] args) {
+	
+		
+		double p1= 60;
+		double p2= 50;
+		double desc;
+		double resulS;
+		
+		int escola;
+		
+		
+		
+				
+				Scanner entrada = new Scanner(System.in);
+				
+		System.out.println("Loja RECEBA DESCONTÃO!");
+		System.out.println("");
+		System.out.println("Escolha seu produto:");
+		System.out.println("");
+		System.out.println("1- Calça: R$60");
+		System.out.println("2- Camiseta: R$50");
+		System.out.println("3- Boné: R$130");
+		System.out.println("4- Sapato: R$500");
+		System.out.println("");
+			escola= Integer.parseInt(entrada.nextLine());
+				
+				
+
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+	}
+
+}
