c495c9cc06035d77d60ef5b92eb7a71aa9132a6a
aluno <aluno@DSN1251017041>
2022-04-13 11:15:30 -0300

    Deus é bom!
    Métodos diferentes de comparação, para algum resultado.
     (variável, e valor boleano)
    
     Troca de figurinhas com if, else/ false, true

--- OrientaçãoObjetoAb/src/Main.java
+++ OrientaçãoObjetoAb/src/Main.java
@@ -7,18 +7,48 @@ public class Main {
 		Pessoas pessoa1 = new Pessoas();
 		
 		 pessoa1.nome = "Victor";		 
-		 pessoa1.numFigurinhas = 1;
+		 pessoa1.numFigurinhas = 1; //Figurinhas de Victor
 		 
-		 pessoa1.receber(3);
+		 pessoa1.receber(10); //Victor recebeu mais figurinhas
 		 
 		  System.out.println(pessoa1.nome);
 		  System.out.println(pessoa1.numFigurinhas);
 		
+		Pessoas pessoa2 = new Pessoas();
 		
 		
+	
+		pessoa2.nome = "Gisele";
+		pessoa2.numFigurinhas = 10; //Figurinhas de Gisele
+		
+		 System.out.println(pessoa2.nome); 
+		 System.out.println(pessoa2.numFigurinhas);	//cópia de endereço, as duas variáveis possuem os mesmos endereços.
 		 
+		 int darFig=12;
 		 
-		
+		 //boolean msg = pessoa1.dar(1, pessoa2);
+		 pessoa1.dar(darFig, pessoa2);
+		 
+		if (darFig<pessoa2.numFigurinhas) { //2 metodos 1-Usar uma variável para armazenar o valor da quantidade de figurinhas.
+													  //2-Usar valor boleano, verdadeiro ou falso
+		 //if (msg==true) {
+		 System.out.println();
+		 System.out.println("Resultado da troca de figurinhas:");
+		 System.out.println();
+		 
+		  System.out.println(pessoa1.nome);
+		  	System.out.println(pessoa1.numFigurinhas);
+		  
+		  System.out.println(pessoa2.nome); 
+			 System.out.println(pessoa2.numFigurinhas);
+		 
+		}
+		  
+		  
+		  
+		  
+		  
+		  
 	}
 
 }
--- OrientaçãoObjetoAb/src/Pessoas.java
+++ OrientaçãoObjetoAb/src/Pessoas.java
@@ -9,17 +9,36 @@ public class Pessoas {
 		
 			//Variável global- Todos os métodos dentro dessa classe vão enxergar ela.
 	
-		this.numFigurinhas +=  numFigurinhas;
-		
+		this.numFigurinhas +=  numFigurinhas;	//soma o valor	// O this é usado para referenciar a um atributo externo fora do metodo. Por exemplo ser dentro de um método se estiver
+													//dois atributos com o mesmo nome oque estiver com a palavra "this" estará se referenciando a um atributo fora do metodo.
 		
 		
 		
 		
 	}
 		
-		void dar() {
+	
+	
+		boolean dar( int numFigurinhas, Pessoas pessoa) {
 			
+			boolean msg;
 			
+			if (numFigurinhas> this.numFigurinhas ){
+				
+				System.out.println("Você não tem figurinhas para somar na vida da " + pessoa.nome);
+				msg = false;
+			}
+			
+			else {
+			
+				this.numFigurinhas -= numFigurinhas;
+				
+			pessoa.receber(numFigurinhas);	
+				msg = true;
+			}
+			
+			return msg;	
+				
 		}
 	
 	
