43cc1ae2201cf52381bd52d060e281cfb8de74fd
aluno <aluno@DSN1251017046>
2022-04-27 11:36:55 -0300

    Desastre

---
+++ ExercicioLampada/src/Lampada.java
@@ -0,0 +1,39 @@
+
+public class Lampada {
+ 
+	private boolean ligada;
+	
+	
+	
+//	public Lampada(String msg) {
+//		
+//		if (ligada==true) {
+//		this.msg = "Lampada ligada";
+//	 }
+//		else {
+//			this.msg = "Lampada desligada";
+//		}
+//		
+//		
+//	}
+
+
+	public Lampada(boolean ligada) { //construtor
+	
+	}
+
+	
+	public void desligar() {
+		this.ligada = false;
+		
+	}
+	
+	public void ligar () {
+		this.ligada = true;
+		
+	}
+	
+	public void imprimir() {
+		
+	}
+}
---
+++ ExercicioLampada/src/main.java
@@ -0,0 +1,23 @@
+public class main {
+
+	public static void main(String[] args) {
+		
+		
+		Lampada lampada = new Lampada(false);
+		
+		
+		lampada.ligar();
+	
+		
+		
+		
+		
+		
+		
+		
+	
+		
+		
+	}
+
+}
