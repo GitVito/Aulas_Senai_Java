d2e124f4e27c494b2d2bf1fcac1a561d98601369
aluno <aluno@DSN1251017054>
2022-05-16 11:28:09 -0300

    a

--- FundamentoJava/src/main.java
+++ FundamentoJava/src/main.java
@@ -122,29 +122,69 @@ public class main {
 		
 		
 		//--___----___Tabuada--___----___\\
-		
-		Scanner a = new Scanner(System.in);
-		
-		System.out.println("Digte o número da tabuada que você deseja:");
-		 int y = a.nextInt();
-		
-		
-		for (int x=0; x<=10; x++) {
+//		
+//		Scanner a = new Scanner(System.in);
+//		
+//		System.out.println("Digte o número da tabuada que você deseja:");
+//		 int y = a.nextInt();
+//		
+//		
+//		for (int x=0; x<=10; x++) {
+//
+//			System.out.println(y + " x " + x + " = " + x*y);
+//		}
+//		
+//		
+//		
 
-			System.out.println(y + " x " + x + " = " + x*y);
-		}
 		
 		
+		int x1 = 3;
+		int y1 = 3;
+		int z1 = 3;
 		
 		
-		
-		
-		
-		
-		
-		
-		
-		
+			System.out.println(x1);
+			System.out.println(y1);
+			System.out.println(z1);
+			
+				double x [] = {3, 5, 3, 5};
+				
+					for (int aux = 0; aux < x.length; aux++) {
+						System.out.println("Valor do indice " + aux + " = " + x[aux]);
+						
+				}
+						String nome[] = { "Rafael", "Gislene", "Lara"};
+						
+					for (int aux1 = 0; aux1 < nome.length; aux1++) {
+						System.out.println(nome[aux1]);
+						 if (nome[aux1] == "Gislene") {
+							 System.out.println("O indice de Gislene é = " + aux1);
+						 }
+					}	
+					
+					
+					int[] arrayInt = new int[5];
+					String[] ArrayString = new String[4];
+					double[] ArrayDouble = new double[3];
+					
+					 arrayInt[3] = 4;
+					 
+					int[] numero = new int[6];
+					Scanner teclado = new Scanner(System.in);
+					
+					
+					for (int aux2 = 0; aux2 < numero.length; aux2 ++) {
+						System.out.println("Digite um número qualquer");
+						 numero[aux2] = teclado.nextInt();
+						 
+					}
+					
+					for (int aux2 = 0; aux2 < numero.length; aux2 ++) {
+						System.out.println("Indice " + aux2 + " = " + numero[aux2]);
+						 numero[aux2] = teclado.nextInt();
+						 
+					}
 		
 			
 		}	
