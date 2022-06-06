4f19b79f7f318ac0cd6b953ebcd1b8e02bad7536
GitVito <102964046+GitVito@users.noreply.github.com>
2022-06-06 08:55:53 -0300

    Update Aplicacao.java
    pequenos comentarios

--- ConstrutorDeHorario/src/Aplicacao.java
+++ ConstrutorDeHorario/src/Aplicacao.java
@@ -4,9 +4,9 @@ public class Aplicacao {
 	public static void main(String[] args) {
 		
 
-		Data d1 = new Data(10, 03, 2000, 0, 30, 10);// A variável foi criada para colocar o endereço no objeto "Data() = É o construtor"
+		Data d1 = new Data(10, 03, 2000, 0, 30, 10);// O objeto foi criado para colocar o endereÃ§o no objeto "Data() = Ã‰ o construtor"
 		 d1.imprimir(Data.FORMATO_12H);
-		 d1.imprimir(Data.FORMATO_24H);
+		 d1.imprimir(Data.FORMATO_24H); //Usando o metodo criado anteriormente
 		 
 		 Data d2 = new Data(15, 06, 2000, 23, 15, 20);
 		 d2.imprimir(Data.FORMATO_12H);
